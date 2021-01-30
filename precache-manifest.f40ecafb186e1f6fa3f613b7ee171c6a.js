bc2f72f4607b4de5852999bd1b48b605b52dd73f
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-01-30 14:41:20 +0530

    Updates

---
+++ precache-manifest.f40ecafb186e1f6fa3f613b7ee171c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e021784d341dee6270f1d355d3dde5",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "856cb43427b74627d432",
+    "url": "/React-Todo-App/static/css/main.b18d740a.chunk.css"
+  },
+  {
+    "revision": "8d21629c41174ceec1c8",
+    "url": "/React-Todo-App/static/js/2.44b8750f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-App/static/js/2.44b8750f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "856cb43427b74627d432",
+    "url": "/React-Todo-App/static/js/main.7cc75a96.chunk.js"
+  },
+  {
+    "revision": "3f8f8ba9087d44353137",
+    "url": "/React-Todo-App/static/js/runtime-main.7ddf3353.js"
+  }
+]);
